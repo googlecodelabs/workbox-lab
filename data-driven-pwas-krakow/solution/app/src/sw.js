e96cdc04cbb878ec22e96a701fe9b1218435b7d8
David Scales <dscales@google.com>
2017-08-02 11:12:43 -0700

    initial commit data-driven-pwa

---
+++ data-driven-pwas-krakow/solution/app/src/sw.js
@@ -0,0 +1,80 @@
+importScripts('workbox-sw.prod.v1.1.0.js');
+
+const workboxSW = new WorkboxSW();
+workboxSW.precache([]);
+
+workboxSW.router.registerRoute('https://fonts.googleapis.com/(.*)',
+  workboxSW.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    cacheExpiration: {
+      maxEntries: 20
+    },
+    cacheableResponse: {statuses: [0, 200]}
+  })
+);
+
+workboxSW.router.registerRoute(/\.(?:png|gif|jpg)$/,
+  workboxSW.strategies.cacheFirst({
+    cacheName: 'images-cache',
+    cacheExpiration: {
+      maxEntries: 50
+    }
+  })
+);
+
+workboxSW.router.registerRoute('http://weloveiconfonts.com/(.*)',
+  workboxSW.strategies.cacheFirst({
+    cacheName: 'iconfonts',
+    cacheExpiration: {
+      maxEntries: 20,
+      maxAgeSeconds: 7 * 24 * 60 * 60
+    },
+    cacheableResponse: {statuses: [0, 200]}
+  })
+);
+
+workboxSW.router.registerRoute('/**/images/icon/*',
+  workboxSW.strategies.staleWhileRevalidate({
+    cacheName: 'icon-cache',
+    cacheExpiration: {
+      maxEntries: 20
+    }
+  })
+);
+
+var articleHandler = workboxSW.strategies.networkFirst({
+  cacheName: 'articles-cache',
+  cacheExpiration: {
+    maxEntries: 50
+  }
+});
+
+workboxSW.router.registerRoute('/**/pages/article*.html', function(event) {
+  return articleHandler.handle(event).then(function(response) {
+    if (!response) {
+      return caches.match('pages/offline.html');
+    } else if (response.status === 404) {
+      return caches.match('pages/404.html');
+    }
+    return response;
+  });
+});
+
+var postHandler = workboxSW.strategies.cacheFirst({
+  cacheName: 'posts-cache',
+  cacheExpiration: {
+    maxEntries: 50
+  }
+});
+
+workboxSW.router.registerRoute('/**/pages/post*.html', function(event) {
+  return postHandler.handle(event).then(function(response) {
+    if (response.status === 404) {
+      return caches.match('pages/404.html');
+    }
+    return response;
+  })
+  .catch(function() {
+    return caches.match('pages/offline.html');
+  });
+});
