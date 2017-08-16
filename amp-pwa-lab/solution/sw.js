949bd02d3e6ec957378ece097749ddc6b0c4d7e8
Nicolas Searle <nsearle@google.com>
2017-08-16 14:10:54 -0700

    reorganized code to match lab instructions

--- amp-pwa-lab/solution/sw.js
+++ amp-pwa-lab/solution/sw.js
@@ -3,8 +3,8 @@ importScripts('workbox-sw.prod.v1.1.0.js');
 const workboxSW = new self.WorkboxSW();
 workboxSW.precache([]);
 
-workboxSW.router.registerNavigationRoute('/shell.html');
-
 workboxSW.router.registerRoute(/cdn\.ampproject\.org/,
   workboxSW.strategies.staleWhileRevalidate()
 );
+
+workboxSW.router.registerNavigationRoute('/shell.html');
