21e7febfecdce93c65f81db49bfd7a54f6f43226
Nicolas Searle <nsearle@google.com>
2017-08-21 10:50:49 -0700

    fix(data-driven-lab): removed commented code
    Removed commented code in data-driven-pwas-krakow/project/gulpfile.js
    
    Change-Id: I5458a3755b43fbc24de95b064e140b2df45c112c

--- data-driven-pwas-krakow/project/gulpfile.js
+++ data-driven-pwas-krakow/project/gulpfile.js
@@ -22,22 +22,3 @@ gulp.task('default', ['clean'], cb => {
 gulp.task('watch', function() {
   gulp.watch('app/**/*', ['default']);
 });
-
-// gulp.task('build-sw', () => {
-//   return wbBuild.injectManifest({
-//     swSrc: 'app/src/sw.js',
-//     swDest: 'build/service-worker.js',
-//     globDirectory: 'app',
-//     staticFileGlobs: [
-//       'style/main.css',
-//       'index.html',
-//       'js/idb-promised.js',
-//       'js/main.js',
-//       'images/*.jpg',
-//       'images/**/*.svg',
-//       'manifest.json'
-//     ]
-//   }).catch((err) => {
-//     console.log('[ERROR] This happened: ' + err);
-//   });
-// });
