d6c8456377b3bae0064e6b28a25f160e268de613
Nicolas Searle <nsearle@google.com>
2017-10-05 13:53:15 -0700

    changed globDirectory to build/

--- solution/workbox-cli-config.js
+++ solution/workbox-cli-config.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 module.exports = {
-  "globDirectory": "src/",
+  "globDirectory": "build/",
   "globPatterns": [
     "**/*.css",
     "index.html",
