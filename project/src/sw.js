c38afd2bf8acebad5258b01eef25324baaf528d4
Michael Lu <michaelwlu@live.com>
2019-08-13 15:22:45 -0600

    Added initial workbox-config.js and service worker

--- project/src/sw.js
+++ project/src/sw.js
@@ -13,3 +13,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
+
+if (workbox) {
+  console.log('WORKBOX INITIALIZED, HUMAN');
+
+  workbox.precaching.precacheAndRoute([]);
+} else {
+  console.log('CATASTROPHIC ERROR: WORKBOX FAILED TO INITIALIZE');
+}
